7194d8a1f197fd79b6d0fdc2d138eeb1d9ccc17f
Cuong La <cuong.la@us.idemia.com>
2024-08-14 22:05:57 -0700

    Update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,14 +7,14 @@ export const config = {
 	port: parseInt((process.env.PORT as string), 10),
 	mongoURL: process.env.MAYAH_SHOP_MONGO_URL as string,
 	logs: {
-		level: process.env.LOG_LEVEL || 'silly',
+		level: process.env.LOG_LEVEL as string || 'silly',
 	},
 	api: {
 		prefix: '/api',
 	},
 	redis: {
-		host: process.env.REDIS_IP,
-		password: process.env.REDIS_PASSWORD,
-		port: process.env.REDIS_PORT
+		host: process.env.REDIS_IP as string,
+		password: process.env.REDIS_PASSWORD as string,
+		port: process.env.REDIS_PORT as string
 	}
 };
